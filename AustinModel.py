f1dbed367cc74fdd73957ac24543cbbecceda08c
sfulay <fulaysuyash@gmail.com>
2016-10-22 15:30:42 -0400

    Add files via upload

---
+++ AustinModel.py
@@ -0,0 +1,19 @@
+from pyAudioAnalysis import audioTrainTest as aT
+
+def classify(classifier,myFile):
+    aT.featureAndTrain(["Austin/a","Austin/f","Austin/h","Austin/u","Austin/c", "Austin/n","Austin/the","Austin/me"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, classifier, classifier, False)
+    print aT.fileClassification(myFile,classifier,classifier)
+    # print aT.fileClassification("Austin/a1.wav", classifier, classifier)
+    # print aT.fileClassification("Austin/f1.wav", classifier,classifier)
+    # print aT.fileClassification("Austin/h1.wav", classifier,classifier)
+    # print aT.fileClassification("Austin/u1.wav", classifier,classifier)
+    # print aT.fileClassification("Austin/c1.wav", classifier,classifier)
+
+def classifyFile(myFile):
+    print aT.fileClassification(myFile, 'svm', 'svm')
+
+# classifiers = ["svm"]
+# for classifier in classifiers:
+#     classify(classifier)
+#classify('svm')
+print type(classify('svm','Austin/me.wav'))
